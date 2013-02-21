4cd70af7929c889d4c5db8ec277de561d0bf8ec2
stimpz0r <stimpz0r@kritical.co>
2013-02-21 22:36:27 +1100

    move kernel options to full_p1.mk

--- full_p1.mk
+++ full_p1.mk
@@ -26,3 +26,7 @@ $(call inherit-product, device/samsung/p1/p1.mk)
 PRODUCT_NAME := full_p1
 PRODUCT_DEVICE := p1
 PRODUCT_MODEL := GT-P1000
+
+TARGET_KERNEL_SOURCE := kernel/samsung/p1
+TARGET_KERNEL_CONFIG := cyanogenmod_p1_defconfig
+
